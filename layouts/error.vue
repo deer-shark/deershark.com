ac4e1ded1913ad664749779351a0106571b38b6b
yc97463 <youualan87@gmail.com>
2022-06-23 21:56:46 +0800

    feat(layouts/error): set up 404 page as IndexPage.vue

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <IndexPage />
+    <!-- <NuxtLink to="/">Home Page</NuxtLink> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+  layout: "error", // you can set a custom layout for the error page
+};
+</script>
