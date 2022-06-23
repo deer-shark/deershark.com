9af36ed92a87b79b5f2052831fd3bcea1b7db84b
yc97463 <youualan87@gmail.com>
2022-06-23 18:12:41 +0800

    feat(v3): redesign deershark.com by using nuxtJs

---
+++ components/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-4">
+    <button class="px-5 py-2 border rounded hover:bg-teal-200">Hi 👋</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <IndexPage/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import IndexPage from '../components/IndexPage.vue'
+
+export default Vue.extend({
+    name: "LandingPage",
+    components: { IndexPage }
+})
+</script>
